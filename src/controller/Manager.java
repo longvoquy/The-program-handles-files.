6c41aea83ee9f4e709afcf4458ffd65957b5b4c7
longvoquyhoang <longvolongvo360@gmail.com>
2023-10-14 13:02:44 +0700

    123

---
+++ src/controller/Manager.java
@@ -0,0 +1,36 @@
+
+package controller;
+
+import common.Library;
+import view.Menu;
+
+
+public class Manager extends Menu<String> {
+
+    private Library library;
+
+    public Manager() {
+        super("Main Menu", new String[]{"Find person info", "Copy text to new file", "Exit"});
+        library = new Library();
+    }
+
+    @Override
+    public void execute(int n) {
+        switch (n) {
+            case 1:
+                library.findPersonInfo();
+                break;
+            case 2:
+                library.coppyNewFile();
+                break;
+            case 3:
+                System.out.println("Exiting the program.");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Invalid option.");
+                break;
+        }
+
+    }
+}
